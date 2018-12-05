08e6c8c92c9860c81cb581c0d94d3ce5904a6456
Patrick Koo <32448400+PatrickKM@users.noreply.github.com>
2018-12-05 17:33:58 +0100

    Behandlinger + Back to Top

--- js/main.js
+++ js/main.js
@@ -4,3 +4,21 @@ document.addEventListener("DOMContentLoaded", function() {
   console.log("Document ready!");
   
 });
+
+
+// Back to top - button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 790; // For Safari
+    document.documentElement.scrollTop = 790; // For Chrome, Firefox, IE and Opera
+};
