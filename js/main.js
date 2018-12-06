618586c7fb2b054ffbca000c90a4d66ac6b07808
Patrick Koo <32448400+PatrickKM@users.noreply.github.com>
2018-12-06 13:27:09 +0100

    footer styling + wp api

--- js/main.js
+++ js/main.js
@@ -1,13 +1,36 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", function() {
-  // the DOM is fully loaded
-  console.log("Document ready!");
-  
+document.addEventListener("DOMContentLoaded", function () {
+    // the DOM is fully loaded
+    console.log("Document ready!");
+
+    // Wordpress API Fetch - nyheder.html
+    fetch('http://hypno.koomunication.dk/wp-json/wp/v2/posts')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            console.log(json);
+            appendPosts(json);
+        });
+
+    function appendPosts(posts) {
+        for (let i = 0; i < posts.length; i++) {
+            let post = posts[i];
+            console.log(post);
+            document.querySelector('#gridPosts').innerHTML += `
+          <article class="gridItem">
+            <h3>${post.title.rendered}</h3>
+            <p>${post.content.rendered}</p>
+          </article>
+        `;
+        }
+    }
 });
 
-
 // Back to top - button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+    scrollFunction()
+};
 
 function scrollFunction() {
     if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
